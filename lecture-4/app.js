2070bd2376b86ebf694b6c245c42156d5602d2b8
kevinhsu <kevinhsu@synology.com>
2018-09-15 00:27:25 +0800

    add log

--- lecture-4/app.js
+++ lecture-4/app.js
@@ -3,23 +3,35 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hit monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
       this.monsterAttack();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hit monster hard for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -31,13 +43,22 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heal for 10'
+      });
       this.monsterAttack();
     },
     giveUp: function() {
       this.gameIsRunning = false;
     },
     monsterAttack: function() {
-      this.playerHealth -=this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hit player for ' + damage
+      });
       this.checkWin()
     },
     calculateDamage: function(min, max) {
