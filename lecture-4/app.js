b91bf97ad105f7d418b7dd0fbb529692f73eaafd
kevinhsu <kevinhsu@synology.com>
2018-09-14 23:57:25 +0800

    add attack

--- lecture-4/app.js
+++ lecture-4/app.js
@@ -6,10 +6,52 @@ new Vue({
     gameIsRunning: false
   },
   methods: {
-    startGame: function () {
+    startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+      this.playerHealth -=this.calculateDamage(5, 12);
+      if (this.checkWin()) {
+        return;
+      }
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! Start new game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+
+      if (this.playerHealth <= 0) {
+        if (confirm('You lost! Start new game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 })
